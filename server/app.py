089f956e17c87008b7c5218f8dde3681c9af6c76
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-08-16 11:18:36 -0600

    finishes lab

--- server/app.py
+++ server/app.py
@@ -22,13 +22,48 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
+    articles = []
+    for article in Article.query.all():
+        article_dict = article.to_dict()
+        articles.append(article_dict)
+    response = make_response(
+        articles,
+        200
+    )
+    return response 
 
-    pass
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-
-    pass
+    article = Article.query.filter(Article.id == id).first()
+    if article == None: 
+        response_body = {
+            "message": "pick an article that actually exists, bro. i mean really...."
+        } 
+        response = make_response(
+            response_body,
+            404
+        )
+        return response 
+    else:
+        session['page_views'] += 1
+        if session['page_views'] <= 3:
+            article_dict = article.to_dict()
+            article_dict["page_views"] = session['page_views']
+            response = make_response(
+                article_dict,
+                200
+            )
+            return response
+        else:
+            response_body = {
+                'message': 'Maximum pageview limit reached'
+                }
+        response = make_response(
+            response_body,
+            401
+        )
+        return response  
 
 if __name__ == '__main__':
     app.run(port=5555)
